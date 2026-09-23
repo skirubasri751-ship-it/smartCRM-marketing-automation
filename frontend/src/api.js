3eaa861a04ffc44b9adde2bf5b7b9c214a499845
skirubasri751-ship-it <skirubasri751@gmail.com>
2026-09-23 22:10:19 +0530

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "https://smartcrm-marketing-automation-1.onrender.com/api"
+    baseURL: "https://smartcrm-marketing-automation-2.onrender.com/api"
 });
 
 export default API;
